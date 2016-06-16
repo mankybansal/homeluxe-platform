c670261ddf00df84fcb75ff1e57ef7c5bb8235d1
Mayank Bansal <sunny.bansal@gmail.com>
2016-06-16 19:57:46 +0530

    User Panel JS Updates. Register working

--- accounts/userPanel.js
+++ accounts/userPanel.js
@@ -18,14 +18,14 @@ function login(username, password) {
             },
             url: "http://homeluxe.in:3000/member/login",
             success: function (data) {
-                if(data.status == "Success"){
+                if (data.status == "Success") {
                     myUser = data;
                     showDashboard();
-                }else{
+                } else {
                     showAlert('Wrong username or password.');
                 }
             },
-            error: function(data){
+            error: function (data) {
                 showAlert("An unknown error occurred.");
             }
         });
@@ -37,7 +37,6 @@ function login(username, password) {
 function showAlert(message) {
     $('.alertMessage').fadeIn(300);
     $('.alertMessage').html(message);
-    $('.loginLoader').hide();
 }
 
 
@@ -52,11 +51,11 @@ $(document).ready(function () {
     $(".loginButton").click(function () {
         var username = $('#username').val();
         var password = $('#password').val();
-        $('.loginLoader').show();
+        showAlert("Please wait... &nbsp; <i class='fa fa-circle-o-notch fa-spin'></i>");
         login(username, password);
     });
 
-    $(".menuOption").click(function(){
+    $(".menuOption").click(function () {
         var menuGroupSelected = $(this).parent().find('.menuGroupLabel').text();
         var menuOptionSelected = $(this).find('.menuOptionText').text();
 
@@ -73,37 +72,80 @@ $(document).ready(function () {
 
 });
 
-function isTokenSet(){
-    if(apiToken){
+function isTokenSet() {
+    if (apiToken) {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 
+function submitRegister() {
+    var rname = $('#regName').val();
+    var remail = $('#regEmail').val();
+    var rphone = $('#regPhone').val();
+    var rpassword = $('#regPassword').val();
 
-function submitRegister(){
-
+    if (rname != "" && remail != "" && rpassword != "" && rphone != "") {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                "token": apiToken,
+                "name": rname,
+                "email": remail,
+                "mobile": rphone,
+                "password": rpassword
+            },
+            url: "http://homeluxe.in:3000/member/register",
+            success: function (data) {
+                if (data.status == "Success") {
+                    showLogin();
+                    showAlert('Account Created. Please Sign-in.');
+                } else if (data.status == "Failed" && data.message == "User already exists") {
+                    showAlert('You already have an account.');
+                }
+                else {
+                    showAlert('Please fill the form correctly.');
+                }
+            },
+            error: function (data) {
+                showAlert("An unknown error occurred.");
+            }
+        });
+    }
 }
 
-function facebookRegister(){
-    
-}
+function showLogin() {
+    $(".loginContainer").animate({"height": "440px"}, 500);
+    $(".registerPanel").hide();
+    setTimeout(function () {
+        $(".loginPanel").fadeIn(500);
+    }, 200)
 
-function userRegister(){
-    $(".loginContainer").animate({"height":"570px"},500);
-    $(".loginPanel").hide();
-    setTimeout(function(){
-        $(".registerPanel").fadeIn(500);
-    },200)
-    
-    
     var spacerHeight = $(".loginSpacer").height();
-    
-    $(".loginSpacer").animate({"height":spacerHeight-65},500);
+
+    $(".loginSpacer").animate({"height": spacerHeight + 65}, 500);
 }
 
-function showDashboard(){
+function facebookRegister() {
+
+}
+
+function userRegister() {
+    $(".loginContainer").animate({"height": "570px"}, 500);
+    $(".loginPanel").hide();
+    setTimeout(function () {
+        $(".registerPanel").fadeIn(500);
+    }, 200)
+
+
+    var spacerHeight = $(".loginSpacer").height();
+
+    $(".loginSpacer").animate({"height": spacerHeight - 65}, 500);
+}
+
+function showDashboard() {
     setTimeout(function () {
         $('.loginOverlay').fadeOut(1000);
     }, 2000);
@@ -111,9 +153,13 @@ function showDashboard(){
     setTimeout(function () {
         $('.contentOverlay').fadeIn(1000);
     }, 3000);
-    
+
     $(".dataName").html(myUser.name);
-    $(".avatarBox").append("<img class='profilePic' src='" + myUser.profile_pic + "'/>");
+    if (myUser.profile_pic) {
+        $(".avatarBox").append("<img class='profilePic' src='" + myUser.profile_pic + "'/>");
+    } else {
+        $(".avatarBox").append("<i class='fa fa-user' style='font-size: 25px; margin-top: 7.5px; margin-left: 10px; color: rgba(0,0,0,0.2);'></i>");
+    }
 }
 
 function getToken() {
@@ -159,11 +205,18 @@ function FBLogin() {
     });
 }
 
+
 function getUserInfo() {
+
+
+    showAlert("Please wait... &nbsp; <i class='fa fa-circle-o-notch fa-spin'></i>");
+
     FB.api('/me?fields=name,picture,email,id,link', function (response) {
         console.log(response.name);
         console.log(response.id);
         console.log(response.email);
+
+
 
         $.ajax({
             type: "POST",
@@ -175,14 +228,14 @@ function getUserInfo() {
             },
             url: "http://homeluxe.in:3000/member/login",
             success: function (data) {
-                if(data.status == "Success"){
+                if (data.status == "Success") {
                     myUser = data;
                     showDashboard();
-                }else{
+                } else {
                     showAlert('Wrong username or password.');
                 }
             },
-            error: function(data){
+            error: function (data) {
                 showAlert("An unknown error occurred.");
             }
         });
@@ -192,6 +245,98 @@ function getUserInfo() {
         console.log(response.data.url);
     });
 }
+
+
+var regFBID, regFBDP;
+
+function facebookRegister() {
+    FB.login(function (response) {
+        console.log(response);
+        if (response.authResponse) {
+
+
+            FB.api('/me?fields=name,picture,email,id,link', function (response) {
+                console.log(response.name);
+                console.log(response.id);
+                console.log(response.email);
+
+                regFBID = response.id;
+
+                $('#regName').css({"background": "#3b5998", "color": "white"}).val(response.name);
+                $('#regEmail').css({
+                    "background": "#3b5998",
+                    "color": "white"
+                }).val(response.email).attr("disabled", "disabled");
+                $('#regPhone').val();
+                $('#regPassword').css({
+                    "background": "#3b5998",
+                    "color": "white"
+                }).val(response.id).attr("disabled", "disabled");
+                showAlert("Verify Information From Facebook");
+            });
+
+            $(".fbRegisterButton").hide();
+            $(".regStep2").hide();
+            $(".fbContButton").show();
+
+            FB.api('/me/picture?type=normal', function (response) {
+                regFBDP = response.data.url;
+            });
+
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, {
+        scope: 'email,public_profile',
+        return_scopes: true
+    });
+}
+
+
+function fbsubmitRegister() {
+
+
+    var rname = $('#regName').val();
+    var remail = $('#regEmail').val();
+    var rphone = $('#regPhone').val();
+    var rpassword = $('#regPassword').val();
+
+
+    if (rname != "" && rphone != "") {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {
+                "token": apiToken,
+                "name": rname,
+                "email": remail,
+                "mobile": rphone,
+                "password": rpassword,
+                "oauth": regFBID,
+                "profile_pic": regFBDP
+            },
+            url: "http://homeluxe.in:3000/member/register",
+            success: function (data) {
+                console.log(data);
+                if (data.status == "Success") {
+                    showLogin();
+                    showAlert('Account Created. Please Sign-in.');
+                } else if (data.status == "Failed" && data.message == "User already exists") {
+                    showAlert('You already have an account.');
+                }
+                else {
+                    showAlert('Please fill the form correctly.');
+                }
+            },
+            error: function (data) {
+                showAlert("An unknown error occurred.");
+            }
+        });
+    } else {
+        showAlert('Please fill the form correctly.');
+    }
+}
+
 
 // Load the SDK asynchronously
 (function (d) {
