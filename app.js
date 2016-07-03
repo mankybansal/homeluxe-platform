487496077bf51c123fad1dd81c04b66229616c56
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-03 20:37:35 +0530

    Angular Test

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 var quizApp = angular.module('quizApp', []);
 var styles;
 
-quizApp.controller("quizController", function ($scope) {
+quizApp.directive("quizApp", function () {
 
     var currentQuestion = 0;
     var myAnswers = [];
     var questions = [];
 
-    $scope.startQuiz = function () {
-        $scope.myProgress = 0;
-        $scope.quizOver = false;
-        $scope.inProgress = true;
-        requests.getQuiz(function (response) {
-            questions = response;
-            $scope.getNextQuestion();
-        });
-    };
+    return {
+        link: function($scope, elem, attrs) {
+            $scope.startQuiz = function () {
+                $scope.myProgress = 0;
+                $scope.quizOver = false;
+                $scope.inProgress = true;
+                requests.getQuiz(function (response) {
+                    questions = response;
+                    $scope.getNextQuestion();
+                });
+            };
 
-    $scope.getNextQuestion = function () {
-        $scope.myProgress += 100 / (questions.length + 1);
-        if (currentQuestion < questions.length)
-            $scope.question = questions[currentQuestion];
-        else {
-            $scope.quizOver = true;
-            requests.submitQuiz(myAnswers.join(), function (response) {
-                styles = response;
-                viewStyle(0);
-            });
+            $scope.getNextQuestion = function () {
+                $scope.myProgress += 100 / (questions.length + 1);
+                if (currentQuestion < questions.length)
+                    $scope.question = questions[currentQuestion];
+                else {
+                    $scope.quizOver = true;
+                    requests.submitQuiz(myAnswers.join(), function (response) {
+                        styles = response;
+                        viewStyle(0);
+                    });
+                }
+                $scope.$apply();
+            };
+
+            $scope.saveAnswer = function (myAnswer) {
+                myAnswers.push(myAnswer);
+                currentQuestion++;
+                $scope.getNextQuestion();
+            };
         }
-        $scope.$apply();
-    };
-
-    $scope.saveAnswer = function (myAnswer) {
-        myAnswers.push(myAnswer);
-        currentQuestion++;
-        $scope.getNextQuestion();
     };
 });
