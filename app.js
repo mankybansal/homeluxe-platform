911c1240c24a8e6ce9fc597dcd5c5732b220eaef
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-03 18:13:25 +0530

    Angular Test

--- app.js
+++ app.js
@@ -1,58 +1,101 @@
-var app = angular.module('quizApp', []);
+var app = angular.module('quizApp', ['ngCookie']);
 var styles;
-app.directive('quiz', function (quizFactory) {
-    return {
-        restrict: 'AE',
-        scope: {},
-        link: function (scope) {
-            scope.start = function () {
-                scope.currentQuestion = 0;
-                scope.myProgress = 0;
-                scope.quizOver = false;
-                scope.inProgress = true;
-                scope.myAnswers = [];
-                scope.getQuestion();
-            };
 
-            scope.getQuestion = function () {
-                scope.myProgress += 100/(quizFactory.questionCount()+1);
-                $('.quizProgress').css('width', scope.myProgress + '%');
-                var q = quizFactory.getQuestion(scope.currentQuestion);
-                if (q) {
-                    scope.question = q.Questions.name;
-                    scope.options = q.Options;
-                } else {
-                    scope.quizOver = true;
-                    requests.submitQuiz(scope.myAnswers.join(),function(response){
-                        styles = response;
-                        viewStyle(0);
-                    });
-                }
-            };
+app.controller("quizController",function($scope){
+    $scope.start = function () {
+        $scope.currentQuestion = 0;
+        $scope.myProgress = 0;
+        $scope.quizOver = false;
+        $scope.inProgress = true;
+        $scope.myAnswers = [];
+        requests.getQuiz(function(response){
+            $scope.questions = response;
+            $scope.getQuestion();
+        });
+    };
 
-            scope.saveAnswer = function (myAnswer) {
-                scope.myAnswers.push(myAnswer);
-                scope.currentQuestion++;
-                scope.getQuestion();
-            };
+    $scope.getQuestion = function () {
+        $scope.myProgress += 100/($scope.questions.length+1);
+        $('.quizProgress').css('width', $scope.myProgress + '%');
+        var q = $scope.getQuestion($scope.currentQuestion);
+        if (q) {
+            $scope.question = q.Questions.name;
+            $scope.options = q.Options;
+        } else {
+            $scope.quizOver = true;
+            requests.submitQuiz($scope.myAnswers.join(),function(response){
+                styles = response;
+                viewStyle(0);
+            });
         }
+    };
+
+    $scope.saveAnswer = function (myAnswer) {
+        $scope.myAnswers.push(myAnswer);
+        $scope.currentQuestion++;
+        $scope.getQuestion();
+    };
+
+    $scope.getQuestion = function (id) {
+        if (id < questions.length) return questions[id];
+        else return false;
     }
 });
 
-app.factory('quizFactory', function () {
-    var questions;
-
-    requests.getQuiz(function(response){
-       questions = response;
-    });
-
-    return {
-        getQuestion: function (id) {
-            if (id < questions.length) return questions[id];
-            else return false;
-        },
-        questionCount: function(){
-            return questions.length;
-        }
-    };
-});
+//
+// app.directive('quiz', function (quizFactory) {
+//     return {
+//         restrict: 'AE',
+//         scope: {},
+//         link: function (scope) {
+//             scope.start = function () {
+//                 scope.currentQuestion = 0;
+//                 scope.myProgress = 0;
+//                 scope.quizOver = false;
+//                 scope.inProgress = true;
+//                 scope.myAnswers = [];
+//                 scope.getQuestion();
+//             };
+//
+//             scope.getQuestion = function () {
+//                 scope.myProgress += 100/(quizFactory.questionCount()+1);
+//                 $('.quizProgress').css('width', scope.myProgress + '%');
+//                 var q = quizFactory.getQuestion(scope.currentQuestion);
+//                 if (q) {
+//                     scope.question = q.Questions.name;
+//                     scope.options = q.Options;
+//                 } else {
+//                     scope.quizOver = true;
+//                     requests.submitQuiz(scope.myAnswers.join(),function(response){
+//                         styles = response;
+//                         viewStyle(0);
+//                     });
+//                 }
+//             };
+//
+//             scope.saveAnswer = function (myAnswer) {
+//                 scope.myAnswers.push(myAnswer);
+//                 scope.currentQuestion++;
+//                 scope.getQuestion();
+//             };
+//         }
+//     }
+// });
+//
+// app.factory('quizFactory', function () {
+//     var questions;
+//
+//     requests.getQuiz(function(response){
+//        questions = response;
+//     });
+//
+//     return {
+//         getQuestion: function (id) {
+//             if (id < questions.length) return questions[id];
+//             else return false;
+//         },
+//         questionCount: function(){
+//             return questions.length;
+//         }
+//     };
+// });
