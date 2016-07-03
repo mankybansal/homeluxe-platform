44f0c22aa58a245e8fcf2f598e78180cf644fd86
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-03 20:54:05 +0530

    Angular Test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('quizApp', []);
 var styles;
 
-app.directive("quizAppLogic", function (quizFactory) {
+app.directive("quizAppLogic", function() {
     return {
         restrict: 'AE',
         scope: {},
@@ -37,46 +37,6 @@ app.directive("quizAppLogic", function (quizFactory) {
                 currentQuestion++;
                 scope.getNextQuestion();
             };
-        }
-    };
-});
-
-app.factory('quizFactory', function() {
-    var questions = [
-        {
-            question: "Which is the largest country in the world by population?",
-            options: ["India", "USA", "China", "Russia"],
-            answer: 2
-        },
-        {
-            question: "When did the second world war end?",
-            options: ["1945", "1939", "1944", "1942"],
-            answer: 0
-        },
-        {
-            question: "Which was the first country to issue paper currency?",
-            options: ["USA", "France", "Italy", "China"],
-            answer: 3
-        },
-        {
-            question: "Which city hosted the 1996 Summer Olympics?",
-            options: ["Atlanta", "Sydney", "Athens", "Beijing"],
-            answer: 0
-        },
-        {
-            question: "Who invented telephone?",
-            options: ["Albert Einstein", "Alexander Graham Bell", "Isaac Newton", "Marie Curie"],
-            answer: 1
-        }
-    ];
-
-    return {
-        getQuestion: function(id) {
-            if(id < questions.length) {
-                return questions[id];
-            } else {
-                return false;
-            }
         }
     };
 });
