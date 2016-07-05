4f05134da508da4ab1f2759f6c086910ac1ad131
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-05 21:36:47 +0530

    Editing App.Js for Dashboard Routes

--- accounts/userPanel.js
+++ accounts/userPanel.js
@@ -15,34 +15,35 @@ homeluxeApp.controller("memberDashboardControl", function ($scope, $rootScope, $
     };
 
 
-    $scope.getLikes = function(){
-        if($scope.$parent.ngMyUser = Cookies.getJSON("myUser"))
-        requests.getLikes($scope.$parent.ngMyUser.token, function(response){
-            $scope.$apply(function () {
-                if(response.success != "false"){
-                    $scope.myLikes = response;
-                }
+    $scope.getLikes = function () {
+        if ($scope.$parent.ngMyUser = Cookies.getJSON("myUser")) {
+            console.log($scope.$parent.ngMyUse);
+            requests.getLikes($scope.$parent.ngMyUser.token, function (response) {
+                $scope.$apply(function () {
+                    if (response.success != "false") {
+                        $scope.myLikes = response;
+                    }
+                });
             });
-        });
+        }
     };
-    
+
     $scope.init();
 });
-
 
 
 $(document).ready(function () {
 
     $('.loginOverlay').show();
-    
-    $(".signOutButton").click(function(){
+
+    $(".signOutButton").click(function () {
         Cookies.remove('myUser');
         hideDashboard();
     });
 
     if (myUser = Cookies.getJSON('myUser')) {
-        showAlert("Signing in as <b>"+myUser.name+"</b> &nbsp; <i class='fa fa-circle-o-notch fa-spin'></i>");
-        setTimeout(showDashboard(),3000);
+        showAlert("Signing in as <b>" + myUser.name + "</b> &nbsp; <i class='fa fa-circle-o-notch fa-spin'></i>");
+        setTimeout(showDashboard(), 3000);
     }
 
     $(".menuOption").click(function () {
@@ -54,15 +55,15 @@ $(document).ready(function () {
 
         $(".menuLeft").find(".optionSelected").removeClass("optionSelected");
         $(this).addClass("optionSelected");
-        
+
         $(".viewPanel").hide();
-        
-        $("#"+ $(this).find(".menuOptionSelect").attr('class').split(' ')[1]).fadeIn(500);
+
+        $("#" + $(this).find(".menuOptionSelect").attr('class').split(' ')[1]).fadeIn(500);
     });
 });
 
 function showDashboard() {
-    
+
     $(".viewPanel").hide();
     $(".menuLeft")
         .find(".optionSelected").removeClass("optionSelected")
@@ -82,13 +83,13 @@ function showDashboard() {
         $(".avatarBox").empty().append("<img class='profilePic' src='" + myUser.profile_pic + "'/>");
     else
         $(".avatarBox").empty().append("<i class='fa fa-user' style='font-size: 25px; margin-top: 7.5px; margin-left: 10px; color: rgba(0,0,0,0.2);'></i>");
-    
-    $(".myDP").append("<img src='"+myUser.profile_pic+"' class='myDPimage'>");
+
+    $(".myDP").append("<img src='" + myUser.profile_pic + "' class='myDPimage'>");
     $(".myName").html(myUser.name);
     $(".myEmail").html(myUser.email);
     $(".myPhone").html(myUser.mobile);
-    
-    if(myUser.fbConnected) $(".connectedTo").html("Connected to Facebook");
+
+    if (myUser.fbConnected) $(".connectedTo").html("Connected to Facebook");
     else $(".connectedTo").html("Not Connected");
 }
 
