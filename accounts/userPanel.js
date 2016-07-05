2cad37398e0b376641e5b4487bc5f49c552cbfdf
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-05 21:58:51 +0530

    Editing App.Js for Dashboard Routes

--- accounts/userPanel.js
+++ accounts/userPanel.js
@@ -17,7 +17,7 @@ homeluxeApp.controller("memberDashboardControl", function ($scope, $rootScope, $
 
 
     $scope.getLikes = function () {
-        if ($scope.$parent.ngMyUser = Cookies.getJSON("myUser")) {
+        if ($scope.$parent.ngMyUser = Cookies.getJSON("myUser") && guestToken) {
             console.log($scope.$parent.ngMyUser);
             requests.getLikes($scope.$parent.ngMyUser.token, function (response) {
                 $scope.$apply(function () {
