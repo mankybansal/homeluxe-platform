8c35925069ad8894ce53aeae39621423dc9ea9cb
Mayank Bansal <sunny.bansal@gmail.com>
2016-07-05 06:27:35 +0530

    Modifying Account Controller

--- js/accounts.js
+++ js/accounts.js
@@ -195,3 +195,41 @@ window.fbAsyncInit = function () {
     js.src = "//connect.facebook.net/en_US/all.js";
     ref.parentNode.insertBefore(js, ref);
 }(document));
+
+
+var styles;
+
+homeluxeApp.controller("quizAppController", function($scope){
+
+    $scope.startQuiz = function(){
+        $scope.currentQuestion = 0;
+        $scope.myAnswers = [];
+        $scope.myProgress = 0;
+        $scope.quizOver = false;
+        $scope.inProgress = true;
+        requests.getQuiz(function(response){
+            $scope.$apply(function () {
+                $scope.questions = response;
+                $scope.getNextQuestion();
+            });
+        });
+    };
+
+    $scope.getNextQuestion = function () {
+        $scope.myProgress += 100 / ($scope.questions.length + 1);
+        if (!($scope.question = $scope.questions[$scope.currentQuestion])) {
+            $scope.quizOver = true;
+            requests.submitQuiz($scope.myAnswers.join(), function (response) {
+                styles = response;
+                viewStyle(0);
+            });
+        }
+    };
+
+    $scope.saveAnswer = function (myAnswer) {
+        $scope.myAnswers.push(myAnswer);
+        $scope.currentQuestion++;
+        $scope.getNextQuestion();
+    };
+
+});
